f0a0b658fcfec1a6e69a68da9c67a26c4d35b0b0
minettebrink <mimmi.brink@gmail.com>
2024-10-29 09:11:16 +0100

    fixes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -29,7 +29,7 @@ var staticFiles embed.FS
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Printf("No .env file found - using environment variables")
 	}
 
 	port := os.Getenv("PORT")
